48d7f218c9e71c6c660569dfc285f3ac768fb9fb
Your Name <olubodekehinde2019@gmail.com>
2026-03-20 20:03:10 +0000

    waiting list page

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Router>
      <Toaster position='top-center'/>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<WaitlistPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path ='/waitlist' element={<WaitlistPage />} />
